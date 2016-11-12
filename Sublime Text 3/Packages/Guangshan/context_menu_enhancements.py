d9869bb884308d83c23e5ad421ce83d41c528712
Guangshan <guangshan1992@qq.com>
2016-11-13 00:07:17 +0800

    Done List 1、合并编辑菜单，虽然可能用的不多，但是右键菜单中出现还是很有必要的 2、替换原有F12对应的OpenInBrowser功能，统一为Open Run命令 3、完成一半的Terminal插件功能
    To Do List
    1、Terminal的另一半功能，在当前项目中打开命令行功能
    2、已打开文件列表功能
    
    Signed-off-by: Guangshan <guangshan1992@qq.com>

--- Sublime Text 3/Packages/Guangshan/context_menu_enhancements.py
+++ Sublime Text 3/Packages/Guangshan/context_menu_enhancements.py
@@ -1,6 +1,10 @@
+# coding=utf8
 import sublime
 import sublime_plugin
+import os
 
+global s
+s = sublime.load_settings('Side Bar.sublime-settings')
 
 class ViewEditToRightCommand(sublime_plugin.WindowCommand):
 	def run(self, paths = []):
@@ -34,3 +38,56 @@ class CopyRelationPathCommand(sublime_plugin.WindowCommand):
 		for directory in sublime.active_window().folders():
 			path2 = path2.replace(directory, '', 1)
 		return path != path2
+
+class ContextOpenCommand(sublime_plugin.WindowCommand):
+	def run(self, paths = []):
+		window = sublime.active_window()
+		path = window.active_view().file_name()
+		dirname, filename = os.path.split(path)
+		if sublime.platform() == 'osx':
+			import subprocess
+			subprocess.Popen(['open', filename], cwd=dirname)
+		elif sublime.platform() == 'windows':
+			import subprocess
+			subprocess.Popen(['start',  '', path.replace("^", "^^")], cwd=dirname, shell=True)
+		else:
+			# linux下可能无效
+			from . import desktop
+			desktop.open(dirname)
+			print('using desktop')
+
+	def is_enabled(self, paths = []):
+		return True
+
+	def is_visible(self, paths =[]):
+		return True
+
+class ContextOpenTerminalCommand(sublime_plugin.WindowCommand):
+	def run(self, paths = []):
+		import subprocess
+		window = sublime.active_window()
+		path = window.active_view().file_name()
+		dirname, filename = os.path.split(path)
+		if sublime.platform() == 'osx':
+			subprocess.Popen(['/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal', '.'], cwd=self.forCwdSystemPath())
+		elif sublime.platform() == 'windows':
+			if s.get('use_powershell', True):
+				try:
+					subprocess.Popen(['start', 'powershell'], cwd=dirname, shell=True)
+				except:
+					subprocess.Popen(['start', 'cmd', '.'], cwd=dirname, shell=True)
+			else:
+				subprocess.Popen(['start', 'cmd', '.'], cwd=dirname, shell=True)
+		elif sublime.platform() == 'linux':
+			subprocess.Popen(['gnome-terminal', '.'], cwd=dirname)
+
+	def is_enabled(self, paths = []):
+		return True
+
+	def is_visible(self, paths =[]):
+		return True
+
+# 隐藏 give20
+class SideBarDonateCommand(sublime_plugin.WindowCommand):
+	def is_visible(self, paths =[]):
+		return False
