43276a7d0d78b8f6d02615ee266ba518d61f0147
Guangshan <159384@sh.lianjia.com>
2016-11-14 19:12:15 +0800

    脚本修改一下，修改一个Key

--- Sublime Text 3/Backup.bat
+++ Sublime Text 3/Backup.bat
@@ -1,10 +1,10 @@
 @echo off
 echo 确认将当前系统的Sublime Text 3配置备份到当前文件夹？
 pause
-set destination=%cd%\Packages\
-set source=C:\Users\Guangshan\AppData\Roaming\Sublime Text 3\Packages\
-xcopy "%source%Default" "%destination%Default\" /s/q/y
-xcopy "%source%Guangshan" "%destination%Guangshan\" /s/q/y
-xcopy "%source%User" "%destination%User\" /s/q/y /exclude:Backup.ignore
+set destination=%cd%\Packages
+set source=%APPDATA%\Sublime Text 3\Packages
+xcopy "%source%\Default" "%destination%\Default\" /s/q/y
+xcopy "%source%\Guangshan" "%destination%\Guangshan\" /s/q/y
+xcopy "%source%\User" "%destination%\User\" /s/q/y /exclude:Backup.ignore
 pause
 exit
--- Sublime Text 3/Restore.bat
+++ Sublime Text 3/Restore.bat
@@ -2,18 +2,18 @@
 echo 确认将当前目录下的Sublime Text 3配置还原到当前系统下？
 pause
 
-set source=%cd%\Packages\
+set source=%cd%\Packages
 
-set destination=C:\Users\Guangshan\AppData\Roaming\Sublime Text 3\Packages\
+set destination=%APPDATA%\Sublime Text 3\Packages
 
 echo 先备份原Packages下所有文件到E盘的Backup中，以防万一
 xcopy "%destination%" "E:\Backup\" /s/q/y
 echo 备份完成
-xcopy "%source%Default" "%destination%Default\" /s/q/y
+xcopy "%source%\Default" "%destination%\Default\" /s/q/y
 
-xcopy "%source%Guangshan" "%destination%Guangshan\" /s/q/y
+xcopy "%source%\Guangshan" "%destination%\Guangshan\" /s/q/y
 
-xcopy "%source%User" "%destination%User\" /s/q/y
+xcopy "%source%\User" "%destination%\User\" /s/q/y
 
 pause
 
